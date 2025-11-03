f5b168556cb7cf5995581297f095ae92690680d7
Alexander-Videv <alex.videv1@gmail.com>
2025-11-03 23:23:20 +0200

    wW

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ import registerRouter from './routes/register.js'
 
 const app = express();
 app.use(cors({
-    origin: ["https://Alexander-Videv.github.io"],
+    origin: ["https://alexander-videv.github.io"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true
 }));
