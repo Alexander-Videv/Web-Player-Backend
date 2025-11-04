3563a326b3b96718b4e7e6230cd4f8ff9d79e0c2
Alexander-Videv <alex.videv1@gmail.com>
2025-11-04 15:01:45 +0200

    t4

--- supabase.js
+++ supabase.js
@@ -4,9 +4,8 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const supabase = createClient(
-    process.env.SUPABASE_URL,
-    process.env.SUPABASE_SERVICE_KEY
-);
+const supabaseUrl = 'https://ctbwchxwetjgpwusodoe.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase;
