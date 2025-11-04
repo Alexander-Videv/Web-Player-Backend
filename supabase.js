237cbd58c80233a87e049645c17fc1f56e552dfe
Alexander-Videv <alex.videv1@gmail.com>
2025-11-04 14:54:08 +0200

    supabase t2

--- supabase.js
+++ supabase.js
@@ -1,9 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
 
-import { createClient } from "@supabase/supabase-js";
-
-const supabase = createClient(
-    process.env.SUPABASE_URL,
-    process.env.SUPABASE_SERVICE_KEY
-);
+const supabaseUrl = 'https://ctbwchxwetjgpwusodoe.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase;
